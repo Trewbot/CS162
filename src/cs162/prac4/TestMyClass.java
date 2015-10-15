14db427be2363412893ac048abeef7ecc17ea5a2
Trewbot <trewbot@phene.co>
2015-10-15 11:45:04 -0500

    Fixed non-static references to static methods.

--- src/cs162/prac4/TestMyClass.java
+++ src/cs162/prac4/TestMyClass.java
@@ -5,12 +5,12 @@ public class TestMyClass {
 				myObj2 = new MyClass(7);
 		//	the errors aren't important
 		myObj1.printX();
-		myObj1.incrementCount();
 		MyClass.incrementCount();
-		myObj1.printCount();
+		MyClass.incrementCount();
+		MyClass.printCount();
 		myObj2.printX();
-		myObj2.printCount();
-		myObj2.incrementCount();
-		myObj1.printCount();
+		MyClass.printCount();
+		MyClass.incrementCount();
+		MyClass.printCount();
 	}
 }
