4d288cab81b9ceaf4f0deff821ed6b4788dc2da9
Trevor Hoglund <trewbot@phene.co>
2015-11-05 12:13:46 -0600

    Added lec12

---
+++ src/cs162/lec12/TwoDimArrays.java
@@ -0,0 +1,9 @@
+package cs162.lec12;
+
+public class TwoDimArrays {
+	public static void main(String[] args){
+		int[][] blue = new int[3][5];
+		char[][] green = new char[5][3];
+		String[][] orange = new String [3][3];
+	}
+}
