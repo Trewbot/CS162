9c33c8ebbb80d94e3a5c2d9fc8dce993ef245b1d
Trevor Hoglund <trewbot@phene.co>
2015-11-05 11:57:23 -0600

    Did whatever

--- src/cs162/lec11/TestPersonWithContacts.java
+++ src/cs162/lec11/TestPersonWithContacts.java
@@ -1,25 +1,25 @@
 package cs162.lec11;
 public class TestPersonWithContacts {
 	public static void main(String[] args){
-		PersonWithContacts me = new PersonWithContacts("Ruxin", "Dai");
-			me.addContacts("Ahmad");
-			me.addContacts("Hossein");
-			me.addContacts("Tony");
-			me.addContacts("Arpan");
-			me.addContacts("Jacob");
-			me.addContacts("Mary-Alice");
+		PersonWithContacts me = new PersonWithContacts("Trevor", "Hoglund");
+			me.addContacts("Real Person");
+			me.addContacts("Real Person");
+			me.addContacts("Real Person");
+			me.addContacts("Real Person");
+			me.addContacts("Real Person");
+			me.addContacts("I Have Friends");
 		me.printAllContacts();
 		System.out.print("\n");
-		String target = "Tony";
+		String target = "Real Person";
 		int res;
 		System.out.println(((res = me.searchContact(target)) < 0
-				? "Search " + target + " fails!"
-				: target + " positions at " +  (res + 1) + " of the contacts."
+				? "Search \"" + target + "\" fails!"
+				: "\"" + target + "\" positions at " +  (res + 1) + " of the contacts."
 		)+ "\n");
-		target = "Jack";
+		target = "Friends";
 		System.out.println(((res = me.searchContact(target)) < 0
-				? "Search " + target + " fails!"
-				: target + " positions at " +  (res + 1) + " of the contacts."
+				? "Search \"" + target + "\" fails!"
+				: "\"" + target + "\" positions at " +  (res + 1) + " of the contacts."
 		)+ "\n");
 		System.out.println(me);
 	}
